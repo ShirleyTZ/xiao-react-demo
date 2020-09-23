33c4fa310f5837887a01c2f627f9f5812f1088fa
ShirleyTZ <553728720@qq.com>
2020-09-23 13:40:32 +0800

    Updates

---
+++ precache-manifest.552f934c188b825962b4b610f5d26769.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc5ea4dbc911517aa9c263b1750b8802",
+    "url": "/xiao-mi-demo/index.html"
+  },
+  {
+    "revision": "60d29389f956b886c809",
+    "url": "/xiao-mi-demo/static/css/main.6274343c.chunk.css"
+  },
+  {
+    "revision": "c74c0812114c07f10cd8",
+    "url": "/xiao-mi-demo/static/js/2.ad042f19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/xiao-mi-demo/static/js/2.ad042f19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60d29389f956b886c809",
+    "url": "/xiao-mi-demo/static/js/main.29b2bdc6.chunk.js"
+  },
+  {
+    "revision": "96eabf03b61c936634e7",
+    "url": "/xiao-mi-demo/static/js/runtime-main.133d872c.js"
+  },
+  {
+    "revision": "332088f796e0ec198fd5a0e6f15835e4",
+    "url": "/xiao-mi-demo/static/media/ApplicationLeft1.332088f7.webp"
+  },
+  {
+    "revision": "6f64dfcccdecce1db8937b9bb5f8e959",
+    "url": "/xiao-mi-demo/static/media/ApplicationLeft2.6f64dfcc.webp"
+  },
+  {
+    "revision": "fedb59bf3650c106e852121700bf7729",
+    "url": "/xiao-mi-demo/static/media/ApplicationRight1.fedb59bf.webp"
+  },
+  {
+    "revision": "ddcc9e4826c1c8f5397cba259a6b844a",
+    "url": "/xiao-mi-demo/static/media/ApplicationRight2.ddcc9e48.webp"
+  },
+  {
+    "revision": "2a237fc729349c2920dcc05b4e6948bb",
+    "url": "/xiao-mi-demo/static/media/ApplicationRight3.2a237fc7.webp"
+  },
+  {
+    "revision": "bcbbf4a4369e26dcb5524948d5348e34",
+    "url": "/xiao-mi-demo/static/media/ApplicationRight5.bcbbf4a4.webp"
+  },
+  {
+    "revision": "7faa74fde2daa8e2c5392683af7baf3b",
+    "url": "/xiao-mi-demo/static/media/ApplicationRight6.7faa74fd.webp"
+  },
+  {
+    "revision": "7c923733c46fbc6fcd37359699636e27",
+    "url": "/xiao-mi-demo/static/media/ApplicationRight7.7c923733.webp"
+  },
+  {
+    "revision": "ac5bb81a0d55a9385cca348818197d9a",
+    "url": "/xiao-mi-demo/static/media/ApplicationRight8.ac5bb81a.webp"
+  },
+  {
+    "revision": "4bca7f55b7c47cc4e1621f09a77156df",
+    "url": "/xiao-mi-demo/static/media/Calorifier.4bca7f55.webp"
+  },
+  {
+    "revision": "b01ebdd0afd9f444e5c2cdc9d299e0b1",
+    "url": "/xiao-mi-demo/static/media/Header.b01ebdd0.jpg"
+  },
+  {
+    "revision": "ef672267fb417cf0d2922b3648664095",
+    "url": "/xiao-mi-demo/static/media/RedMiA9.ef672267.webp"
+  },
+  {
+    "revision": "0e288ec41c0fe7474a78e3d11a4d2dca",
+    "url": "/xiao-mi-demo/static/media/RedMiK30.0e288ec4.jpg"
+  },
+  {
+    "revision": "5e1d17f632e521972ed8de2747e75455",
+    "url": "/xiao-mi-demo/static/media/RedMiK30Pro.5e1d17f6.webp"
+  },
+  {
+    "revision": "51d4f724a96704ae5ce5fe28cfc6980a",
+    "url": "/xiao-mi-demo/static/media/RedMiK30ProBianJiaoBan.51d4f724.webp"
+  },
+  {
+    "revision": "56f1f92f66bdb75be8268315adf9cd6b",
+    "url": "/xiao-mi-demo/static/media/RedMiK30ZhiZunBan.56f1f92f.webp"
+  },
+  {
+    "revision": "c84c8ec3229729a986fc452c625df530",
+    "url": "/xiao-mi-demo/static/media/RedMiPro.c84c8ec3.webp"
+  },
+  {
+    "revision": "e077b7d72e80703bb68f186c3f4a2f16",
+    "url": "/xiao-mi-demo/static/media/RedMiTV.e077b7d7.webp"
+  },
+  {
+    "revision": "95f8458bbe6bf9fbe982f496b8563377",
+    "url": "/xiao-mi-demo/static/media/TVMaster.95f8458b.webp"
+  },
+  {
+    "revision": "d004c85675088f9ec678cd6829a45e80",
+    "url": "/xiao-mi-demo/static/media/XiaoMiWatch.d004c856.webp"
+  },
+  {
+    "revision": "0f1bef6f7dbfb2018a5d7c6e253a0c61",
+    "url": "/xiao-mi-demo/static/media/bannerImg1.0f1bef6f.webp"
+  },
+  {
+    "revision": "37e3a3e7a06095facd72eea4108e6899",
+    "url": "/xiao-mi-demo/static/media/bannerImg2.37e3a3e7.webp"
+  },
+  {
+    "revision": "20142ad495a65cd24a52021a3f0a4a2e",
+    "url": "/xiao-mi-demo/static/media/bannerImg3.20142ad4.webp"
+  },
+  {
+    "revision": "67f4664c6b158f916472417b0a40e831",
+    "url": "/xiao-mi-demo/static/media/bannerImg4.67f4664c.webp"
+  },
+  {
+    "revision": "9be9990e1728a624e60f856f36eb9e45",
+    "url": "/xiao-mi-demo/static/media/bannerImg5.9be9990e.webp"
+  },
+  {
+    "revision": "b97f752764cf3eca6aa8b265f75e58e2",
+    "url": "/xiao-mi-demo/static/media/bannerImg6.b97f7527.webp"
+  },
+  {
+    "revision": "9112557f49731b88cd53778a16a9d8b9",
+    "url": "/xiao-mi-demo/static/media/bannerImg7.9112557f.webp"
+  },
+  {
+    "revision": "5e86431c178a1ad758861c58f96462f6",
+    "url": "/xiao-mi-demo/static/media/download.5e86431c.bin"
+  },
+  {
+    "revision": "e22cb87f627d008572770604bb614311",
+    "url": "/xiao-mi-demo/static/media/imgR1.e22cb87f.jpg"
+  },
+  {
+    "revision": "f04b0fd6777221e95d081ffcee6a22e6",
+    "url": "/xiao-mi-demo/static/media/imgR2.f04b0fd6.jpg"
+  },
+  {
+    "revision": "e332c614dd3fed3e64827f40df40ec80",
+    "url": "/xiao-mi-demo/static/media/imgR3.e332c614.jpg"
+  },
+  {
+    "revision": "b81cf159d3a03faa2784b3327ed73800",
+    "url": "/xiao-mi-demo/static/media/liangPianLong.b81cf159.jpg"
+  },
+  {
+    "revision": "0706a936207f285ec7bd8fa4e23b19d5",
+    "url": "/xiao-mi-demo/static/media/phoneleft.0706a936.webp"
+  },
+  {
+    "revision": "8a035e68f927d075e9bf5b660a2047cb",
+    "url": "/xiao-mi-demo/static/media/tengXunHeiSha3S.8a035e68.webp"
+  },
+  {
+    "revision": "e534485358d353c2f82b069467878ef2",
+    "url": "/xiao-mi-demo/static/media/xiaomi10.e5344853.webp"
+  },
+  {
+    "revision": "f57ee1981782e1d82e954c432c14245a",
+    "url": "/xiao-mi-demo/static/media/xiaomi10QingChun5G.f57ee198.webp"
+  },
+  {
+    "revision": "81b143cfa54822759b7d99fd1aae129a",
+    "url": "/xiao-mi-demo/static/media/xiaomi10ZhiZunBan.81b143cf.webp"
+  }
+]);
